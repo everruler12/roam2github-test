bce889b03d4655cdc8fd3488c7de3344cee3d026
everruler12 <erik.newhard@gmail.com>
2021-01-31 14:06:19 -0700

    EDN formatting working!

--- edn_formatter/src/edn_formatter/core.cljs
+++ edn_formatter/src/edn_formatter/core.cljs
@@ -1,7 +1,27 @@
 (ns edn_formatter.core
   (:require [clojure.pprint :as pp]
-            ;;[clojure.edn :as edn]));; https://github.com/anmonteiro/lumo/issues/419
+            ;;[clojure.edn :as edn])) ;; https://github.com/anmonteiro/lumo/issues/419
             [cljs.reader :as edn]))
 
 (defn ^:export format [^String str]
   (with-out-str (pp/pprint (edn/read-string str))))
+
+;; pprint
+;;  https://clojuredocs.org/clojure.pprint/pprint
+
+;; export
+;;  https://clojurescript.org/reference/advanced-compilation#access-from-javascript
+;;  https://stackoverflow.com/a/26935889
+;;  redundant? https://stackoverflow.com/a/31643787
+
+;; read-string
+;;  https://clojuredocs.org/clojure.edn/read-string
+;;  use cljs.reader instead, see above
+
+;; with-out-str
+;;  https://stackoverflow.com/questions/32107313/pretty-print-to-a-string-in-clojurescript
+
+
+;; build
+;; lumo --classpath src build.cljs
+;;  https://anmonteiro.com/2017/02/compiling-clojurescript-projects-without-the-jvm/
